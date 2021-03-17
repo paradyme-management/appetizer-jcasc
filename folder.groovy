2ec5019525d58f5f90657a01b5dd49d8143800a7
anthony.zawacki <anthony.zawacki@paradyme.us>
2021-03-17 14:34:40 -0400

    Initial cut at getting appetizer in the system

---
+++ folder.groovy
@@ -0,0 +1,70 @@
+String jenkinsTemplateLibraryUrl = 'https://github.com/paradyme-management/jte-the-basics.git'
+String githubCredId = 'jenkins-library-access'
+folder("appetizer") {
+    properties {
+        templateConfigFolderProperty {
+            tier {
+                librarySources {
+                    librarySource {
+                        libraryProvider {
+                            scmLibraryProvider {
+                                baseDir('libraries')
+                                scm {
+                                    gitSCM {
+                                        userRemoteConfigs {
+                                            userRemoteConfig {
+                                                url(jenkinsTemplateLibraryUrl)
+                                                name('origin')
+                                                refspec('+refs/heads/*:refs/remotes/origin/*')
+                                                credentialsId(githubCredId)
+                                            }
+                                        }
+                                        browser {
+                                            githubWeb {
+                                                repoUrl('https://github.com/paradyme-management/jte-the-basics')
+                                            }
+                                        }
+                                        gitTool('/usr/bin/git')
+                                        branches {
+                                            branchSpec {
+                                                name ('*/jte-2.0')
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                configurationProvider {
+                    scmPipelineConfigurationProvider {
+                        baseDir('appetizer')
+                        scm {
+                            gitSCM {
+                                userRemoteConfigs {
+                                    userRemoteConfig {
+                                        url(jenkinsTemplateLibraryUrl)
+                                        name('origin')
+                                        refspec('+refs/heads/*:refs/remotes/origin/*')
+                                        credentialsId(githubCredId)
+                                    }
+                                }
+                                browser {
+                                    githubWeb {
+                                        repoUrl('https://github.com/paradyme-management/jte-the-basics')
+                                    }
+                                }
+                                gitTool('/usr/bin/git')
+                                branches {
+                                    branchSpec {
+                                        name ('*/jte-2.0')
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ jobs.groovy
@@ -0,0 +1,150 @@
+String jobFolderName = 'appetizer'
+String orgName = 'paradyme-management'
+String applicationGithubCredId = 'appetizer-github-credentials'
+
+multibranchPipelineJob("$jobFolderName/appetizer-server") {
+    branchSources {
+        github {
+            id(UUID.randomUUID().toString())
+            scanCredentialsId(applicationGithubCredId)
+            checkoutCredentialsId(applicationGithubCredId)
+            repoOwner(orgName)
+            repository("appetizer-server")
+            buildOriginBranch(true)
+            buildOriginBranchWithPR(true)
+            buildOriginPRHead(true)
+            buildOriginPRMerge(true)
+        }
+    }
+    configure {
+        it / 'projectFactories' << 'org.boozallen.plugins.jte.job.TemplateMultiBranchProjectFactory' {
+        }
+        it / factory(class: 'org.boozallen.plugins.jte.job.TemplateBranchProjectFactory') {
+            filterBranches(true)
+        }
+    }
+    orphanedItemStrategy {
+        discardOldItems {
+            numToKeep(3)
+            daysToKeep(3)
+        }
+    }
+}
+
+multibranchPipelineJob("$jobFolderName/appetizer-ui-image") {
+    branchSources {
+        github {
+            id(UUID.randomUUID().toString())
+            scanCredentialsId(applicationGithubCredId)
+            checkoutCredentialsId(applicationGithubCredId)
+            repoOwner(orgName)
+            repository("appetizer-server")
+            buildOriginBranch(true)
+            buildOriginBranchWithPR(true)
+            buildOriginPRHead(true)
+            buildOriginPRMerge(true)
+        }
+    }
+    configure {
+        it / 'projectFactories' << 'org.boozallen.plugins.jte.job.TemplateMultiBranchProjectFactory' {
+        }
+        it / factory(class: 'org.boozallen.plugins.jte.job.TemplateBranchProjectFactory') {
+            filterBranches(true)
+        }
+    }
+    orphanedItemStrategy {
+        discardOldItems {
+            numToKeep(3)
+            daysToKeep(3)
+        }
+    }
+}
+
+multibranchPipelineJob("$jobFolderName/appetizer-server-image") {
+    branchSources {
+        github {
+            id(UUID.randomUUID().toString())
+            scanCredentialsId(applicationGithubCredId)
+            checkoutCredentialsId(applicationGithubCredId)
+            repoOwner(orgName)
+            repository("appetizer-server")
+            buildOriginBranch(true)
+            buildOriginBranchWithPR(true)
+            buildOriginPRHead(true)
+            buildOriginPRMerge(true)
+        }
+    }
+    configure {
+        it / 'projectFactories' << 'org.boozallen.plugins.jte.job.TemplateMultiBranchProjectFactory' {
+        }
+        it / factory(class: 'org.boozallen.plugins.jte.job.TemplateBranchProjectFactory') {
+            filterBranches(true)
+        }
+    }
+    orphanedItemStrategy {
+        discardOldItems {
+            numToKeep(3)
+            daysToKeep(3)
+        }
+    }
+}
+
+multibranchPipelineJob("$jobFolderName/appetizer-ui") {
+    branchSources {
+        github {
+            id(UUID.randomUUID().toString())
+            scanCredentialsId(applicationGithubCredId)
+            checkoutCredentialsId(applicationGithubCredId)
+            repoOwner(orgName)
+            repository("appetizer-server")
+            buildOriginBranch(true)
+            buildOriginBranchWithPR(true)
+            buildOriginPRHead(true)
+            buildOriginPRMerge(true)
+        }
+    }
+    configure {
+        it / 'projectFactories' << 'org.boozallen.plugins.jte.job.TemplateMultiBranchProjectFactory' {
+        }
+        it / factory(class: 'org.boozallen.plugins.jte.job.TemplateBranchProjectFactory') {
+            filterBranches(true)
+        }
+    }
+    orphanedItemStrategy {
+        discardOldItems {
+            numToKeep(3)
+            daysToKeep(3)
+        }
+    }
+}
+
+multibranchPipelineJob("$jobFolderName/appetizer-helm") {
+    branchSources {
+        github {
+            id(UUID.randomUUID().toString())
+            scanCredentialsId(applicationGithubCredId)
+            checkoutCredentialsId(applicationGithubCredId)
+            repoOwner(orgName)
+            repository("appetizer-server")
+            buildOriginBranch(true)
+            buildOriginBranchWithPR(true)
+            buildOriginPRHead(true)
+            buildOriginPRMerge(true)
+        }
+    }
+    configure {
+        it / 'projectFactories' << 'org.boozallen.plugins.jte.job.TemplateMultiBranchProjectFactory' {
+        }
+        it / factory(class: 'org.boozallen.plugins.jte.job.TemplateBranchProjectFactory') {
+            filterBranches(true)
+        }
+    }
+    orphanedItemStrategy {
+        discardOldItems {
+            numToKeep(3)
+            daysToKeep(3)
+        }
+    }
+}
+
+
